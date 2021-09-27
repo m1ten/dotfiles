c108288b1bda9402a664fee1221671353427d978
miten <57693631+m1ten@users.noreply.github.com>
2021-09-26 21:08:48 -0400

    update

---
+++ scripts/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import platform
+import distro as distribution
+
+if not os.environ['VIRTUAL_ENV']:
+	pass
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-nc", "--noconfirm",
+                    help="yes to everything, don't confirm", action="store_true")
+parser.add_argument("-v", "--verbose", help="print logs", action="store_true")
+args = parser.parse_args()
+
+os = platform.system()
+#distro = distribution.name() if os == 'Linux' else False
+
+# Dependencies
--- scripts/prereq.ps1
+++ scripts/prereq.ps1
@@ -1,6 +1,44 @@
+# Supports both Unix and Windows Systems.
+# '.ps1' extension is required to run PowerShell on Windows.
+# 'chmod +x prereq.ps1 && ./prereq.ps1' on Unix.
+# 'PowerShell .\prereq.ps1' on Windows.
+
+echo @'
+' > /dev/null
+
+# -- Bash --
+ 
+if ! command -v python3; then
+	if [ -x "$(command -v apt)" ]; then
+    	sudo apt install python3
+  	elif [ -x "$(command -v dnf)" ]; then
+    	sudo dnf install python3.9
+  	elif [ -x "$(command -v pacman)" ]; then
+    	sudo pacman -S python
+  	elif [ "$(uname)" = "Darwin" ]; then
+		xcode-select --install
+	fi
+fi
+
+sudo python3 -m venv env --without-pip
+. ./env/bin/activate
+curl -LO 
+
+
+# -- Bash --
+
+echo > /dev/null <<"out-null" ###
+'@ | out-null
+
+# -- PowerShell -- 
+ 
 $version = "3.9.7"
 $url = "https://www.python.org/ftp/python/$version/python-$version-amd64.exe"
 $outpath = "$PSScriptRoot/python-$version.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 Start-Process -Filepath $outpath -Wait
 Remove-Item $outpath
+ 
+# -- PowerShell --
+
+out-null
